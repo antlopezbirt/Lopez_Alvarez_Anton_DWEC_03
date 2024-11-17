88305b270e516de1082dc605d732d2c3ec700a1a
Antón López <antlopez@birt.eus>
2024-11-17 23:17:26 +0100

    Versión mejorada de comprobarVictoria. Convertidos todos los índices en bidimensionales.

--- js/script.js
+++ js/script.js
@@ -6,60 +6,78 @@ var casillero = [
     [0, 0, 0],
     [0, 0, 0]
 ]
+const casillas = document.querySelectorAll("div[id^='cas']");
 var terminado = false;
 var turno = 0;
-var jugadaGanadora;
+var jugadaGanadora = [];
 
+/****************************** FUNCIONES ***********************************/
+
+// Función que comprueba si la última jugada ha conseguido un tres en raya.
 function comprobarVictoria(puntero) {
-    // Buscamos la existencia de una jugada ganadora comparando los arrays (por valor).
-    let tresEnRaya = JSON.stringify([puntero, puntero, puntero]);
-    switch (true) {
-        // Primera fila
-        case (JSON.stringify(casillero[0]) === tresEnRaya):
-            jugadaGanadora = [0, 1, 2];
-            return true;
-        // Segunda fila
-        case (JSON.stringify(casillero[1]) === tresEnRaya):
-            jugadaGanadora = [3, 4, 5];
-            return true;
-        // Tercera fila
-        case (JSON.stringify(casillero[2]) === tresEnRaya):
-            jugadaGanadora = [6, 7, 8];
-            return true;
-        // Primera columna
-        case (JSON.stringify([casillero[0][0], casillero[1][0], casillero[2][0]]) === tresEnRaya):
-            jugadaGanadora = [0, 3, 6];
-            return true;
-        // Segunda columna
-        case (JSON.stringify([casillero[0][1], casillero[1][1], casillero[2][1]]) === tresEnRaya):
-            jugadaGanadora = [1, 4, 7];
-            return true;
-        // Tercera columna
-        case (JSON.stringify([casillero[0][2], casillero[1][2], casillero[2][2]]) === tresEnRaya):
-            jugadaGanadora = [2, 5, 8];
-            return true;
-        // Primera diagonal
-        case (JSON.stringify([casillero[0][0], casillero[1][1], casillero[2][2]]) === tresEnRaya):
-            jugadaGanadora = [0, 4, 8];
-            return true;
-        // Segunda diagonal
-        case (JSON.stringify([casillero[0][2], casillero[1][1], casillero[2][0]]) === tresEnRaya):
-            jugadaGanadora = [2, 4, 6];
-            return true;
+
+    let resultados = 0;
+    let longitud = casillero[0].length; // Al abstraernos de la longitud, nos valdría con cualquiera, siempre que sea impar.
+
+    // Buscamos el nº de ocurrencias en cada fila, iteramos las filas y filtramos los valores para contar resultados.
+    for (let fila = 0; fila < longitud; fila++) {
+        for (let col = 0; col < longitud; col++)
+            // Transforma las posiciones bidimensionales a la numeración de las casillas.
+            jugadaGanadora[col] = ''+ fila + col;
+
+        // Filtra los valores de esa fila y cuenta las ocurrencias.
+        resultados = casillero[fila].filter((v) => (v === puntero)).length;
+
+        // Si son más de dos, existe un tres en raya en esta posición.
+        if (resultados > 2) return true;
     }
+
+    // Para filtrar los valores de las columnas usamos map:
+    // https://stackoverflow.com/questions/7848004/get-column-from-a-two-dimensional-array
+    for (let col = 0; col < longitud; col++) {
+        for (let fila = 0; fila < longitud; fila++)
+            // Transforma las posiciones bidimensionales a la numeración de las casillas correlativas.
+            jugadaGanadora[fila] = ''+ fila + col;
+
+        resultados =  casillero.map((valor,indice) => valor[col]).filter((v) => (v === puntero)).length;
+        if (resultados > 2) return true;
+    }
+
+    // Los valores de las diagonales también se filtrarán con map:
+    // https://stackoverflow.com/questions/67168295/how-do-i-get-the-diagonal-values-in-an-array
+
+    // Diagonal 1
+    for (let diag = 0; diag < longitud; diag++)
+        // Transforma las posiciones bidimensionales a la numeración de las casillas correlativas.
+        jugadaGanadora[diag] = '' + diag + diag;
+
+    resultados =  casillero.map((valor, indice) => valor[indice]).filter((v) => (v === puntero)).length;
+    if (resultados > 2) return true;
+
+    // Diagonal 2
+    for (let diag = 0; diag < longitud; diag++) {
+        // Transforma las posiciones bidimensionales a la numeración de las casillas correlativas.
+        let col = (longitud - diag - 1);
+        jugadaGanadora[diag] = '' + diag + col;
+    }
+    console.log('Diagonal 2: ', jugadaGanadora);
+
+    resultados =  [...casillero].reverse().map((valor, indice) => valor[indice]).filter((v) => (v === puntero)).length;
+    if (resultados > 2) return true;
+
     return false;
 }
 
 function pulsar(casilla)  {
     if (terminado === false) {
-        let fila = Math.trunc(parseInt(casilla.id.charAt(3)) / 3);
-        let columna = parseInt(casilla.id.charAt(3)) % 3;
-        console.log('Fila: ' + fila);
-        console.log('Columna: ' + columna);
+        console.log(casilla.id);
+        // Transformamos los valores correlativos de los ID de las casillas a valores de filas y columnas para acceder al array del casillero
+        let fila = parseInt(casilla.id.charAt(3));
+        let columna = parseInt(casilla.id.charAt(4));
         
         if (casillero[fila][columna] === 0) {
             casillero[fila][columna] = puntero;
-            casilla.innerHTML = '<div id="ficha' + casilla.id.charAt(3) + '" class="' + puntero + '"></div>';
+            casilla.innerHTML = '<div id="ficha' + casilla.id.charAt(3) + casilla.id.charAt(4) + '" class="' + puntero + '"></div>';
 
             if (comprobarVictoria(puntero) === false) {
                 puntero = (puntero === "cruz") ? "circulo" : "cruz";
@@ -85,11 +103,13 @@ function pulsar(casilla)  {
                 terminado = true;
             } else if (puntero === "circulo" && terminado === false) { // Turno del ordenador, si el tablero no está lleno
 
-                let seleccionPC;
+                let seleccionFilaPC;
+                let seleccionColumnaPC;
                 do {
-                    seleccionPC = Math.floor(Math.random()*9);
-                    console.log(seleccionPC);
-                } while (pulsar(document.getElementById('cas' + seleccionPC)) === false);
+                    seleccionFilaPC = Math.floor(Math.random()*3);
+                    seleccionColumnaPC = Math.floor(Math.random()*3);
+                    //console.log(seleccionPC);
+                } while (pulsar(document.getElementById('cas' + seleccionFilaPC + seleccionColumnaPC)) === false);
             }
 
             return true;
@@ -100,9 +120,7 @@ function pulsar(casilla)  {
 }
 
 
-var casillas = document.getElementsByClassName('casilla');
-
-//console.log(casillas);
+/****************************** EVENTOS ***********************************/
 
 for (let casilla of casillas) {
     casilla.addEventListener("click", function() {
@@ -113,6 +131,8 @@ for (let casilla of casillas) {
 var btnJugar = document.querySelector('#jugar');
 var btnSalir = document.querySelector('#salir');
 
+
+// Reinicia los valores y vacía el casillero
 btnJugar.addEventListener("click", function() {
     //window.location.reload(); // opcion facil
 
@@ -124,15 +144,9 @@ btnJugar.addEventListener("click", function() {
     ]
     terminado = false;
     turno = 0;
-
-    for (let i = 0; i < 9; i++) document.querySelector('#cas' + i).innerHTML = '';
+    
+    for (let casilla of casillas) casilla.innerHTML = '';
     document.getElementById('resultado').innerHTML = '';
     document.getElementById('opciones').style.visibility = "hidden";
-
-
-    /*for (let cas of casillero) {
-        console.log('cas' + cas);
-        document.getElementById('cas' + cas).innerHTML = '';
-    }*/
 
 });
