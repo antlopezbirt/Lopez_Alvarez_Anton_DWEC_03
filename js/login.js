2b5c83c7c58019057502267d80e20f07a86c400f
Antón López <antlopez@birt.eus>
2024-12-18 15:12:17 +0100

    Implementado regex contraseña

--- js/login.js
+++ js/login.js
@@ -28,6 +28,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     // Inicializar eventos del formulario
 
     btnEntrar.disabled = true;
+
     
     txtUsuario.addEventListener('input', chequearBotonEntrar);
     txtPass.addEventListener('input', chequearBotonEntrar);
@@ -67,12 +68,69 @@ async function cargarJSON(path) {
     return await response.json();
 }
 
-// Si el campo de usuario o el de contraseña están vacíos, se inhabilita el botón
+// Comprueba si los caracteres introducidos en el campo de contraseña cumplen la restricción de alfanuméricos
+function chequearCaracteres() {
+    
+}
+
+// Comprueba si debe mostrar el ojo del campo de contraseña o no, y lo hace
+function chequearMostrarOjo() {
+    let fieldsetPass= document.getElementById('fldstPass');
+    let spanOjo = null;
+
+    if (txtPass.value != '') {
+        if (!document.getElementById('txtOjo')) {
+            spanOjo = document.createElement('span');
+            spanOjo.setAttribute('id', 'txtOjo');
+            spanOjo.setAttribute('name', 'txtOjo');
+
+            // Muesta el ojo y registra el evento click para mostrar/ocultar el contenido de la contaseña
+            fieldsetPass.appendChild(spanOjo);
+
+            spanOjo.addEventListener('click', function() {
+                if (txtPass.type === 'password') txtPass.type = 'text';
+                else txtPass.type = 'password';
+            });
+        }
+
+        return true;
+    } else {
+        // Si el campo de contraseña se vacía y el usuario pincha fuera, eliminamos el ojo y volvemos a tipo password
+        if (document.getElementById('txtOjo') && document.activeElement != txtPass) {
+            
+            // Elimina el ojo
+            document.getElementById('txtOjo').remove();
+
+            // Preventivamente cambia el tipo del campo a password de nuevo
+            txtPass.type = 'password';
+        }
+    }
+}
+
+// Si el campo de usuario o el de contraseña están vacíos, o si la contraseña tiene caracteres no válidos, se inhabilita el botón y devuelve false
 function chequearBotonEntrar() {
     const usuario = txtUsuario.value;
     const pass = txtPass.value;
-    
-    btnEntrar.disabled = !(usuario && pass);
+    const regex = new RegExp('^[a-z0-9]*$', 'i');
+
+    eliminarErrorLogin();
+
+    // Si la contraseña no es válida, desactiva el botón para evitar más comprobaciones
+    if (!regex.test(pass)) {
+        btnEntrar.disabled = true;
+        mostrarErrorLogin('invalid');
+        return false;
+
+    // Si alguno de los campos está vacío, desactiva el botón igualmente
+    } else if (usuario === '' || pass === '') {
+        btnEntrar.disabled = true;
+        return false;
+
+    // En el resto de casos el botón estará activo
+    } else {
+        btnEntrar.disabled = false;
+        return true;
+    }
 }
 
 function chequearUsuario(event) {
@@ -123,14 +181,30 @@ function chequearUsuario(event) {
 // Muestra un error de login según el tipo que sea
 function mostrarErrorLogin(tipo) {
 
+    eliminarErrorLogin();
+
     // Crea los elementos necesarios para mostrar el error
     spanErrorLogin = document.createElement("span");
     spanErrorLogin.setAttribute("id", "txtErrorLogin");
     spanErrorLogin.setAttribute("name", "txtErrorLogin");
 
-    let textoErrorLogin = ""
-    if (tipo === "user") textoErrorLogin = document.createTextNode("Login incorrecto");
-    else if (tipo ==="pass") textoErrorLogin = document.createTextNode("Contraseña incorrecta");
+    let texto = '';
+
+    switch(tipo) {
+        case 'user':
+            texto = 'Usuario incorrecto';
+            txtUsuario.classList.add('invalido');
+            break;
+        case 'pass':
+            texto = 'Contraseña incorrecta';
+            txtPass.classList.add('invalido');
+            break;
+        case 'invalid':
+            texto = 'Contraseña inválida. Introduce solo letras y números.';
+            txtPass.classList.add('invalido');
+    }
+
+    let textoErrorLogin = document.createTextNode(texto);
 
     spanErrorLogin.appendChild(textoErrorLogin);
     document.getElementById('mainEntrada').appendChild(spanErrorLogin);
@@ -139,39 +213,6 @@ function mostrarErrorLogin(tipo) {
 function eliminarErrorLogin() {
     // Si había algún mensaje de error lo elimina
     if (spanErrorLogin != null) spanErrorLogin.remove();
-}
-
-
-// Comprueba si debe mostrar el ojo del campo de contraseña o no, y lo hace
-function chequearMostrarOjo() {
-    let fieldsetPass= document.getElementById('fldstPass');
-    let spanOjo = null;
-
-    if (txtPass.value != '') {
-        if (!document.getElementById('txtOjo')) {
-            spanOjo = document.createElement('span');
-            spanOjo.setAttribute('id', 'txtOjo');
-            spanOjo.setAttribute('name', 'txtOjo');
-
-            // Muesta el ojo y registra el evento click para mostrar/ocultar el contenido de la contaseña
-            fieldsetPass.appendChild(spanOjo);
-
-            spanOjo.addEventListener('click', function() {
-                if (txtPass.type === 'password') txtPass.type = 'text';
-                else txtPass.type = 'password';
-            });
-        }
-
-        return true;
-    } else {
-        // Si el campo de contraseña se vacía y el usuario pincha fuera, eliminamos el ojo y volvemos a tipo password
-        if (document.getElementById('txtOjo') && document.activeElement != txtPass) {
-            
-            // Elimina el ojo
-            document.getElementById('txtOjo').remove();
-
-            // Preventivamente cambia el tipo del campo a password de nuevo
-            txtPass.type = 'password';
-        }
-    }
+    txtUsuario.classList.remove('invalido');
+    txtPass.classList.remove('invalido');
 }
